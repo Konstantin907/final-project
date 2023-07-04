37d4774d3ac500abda29c135e812c47518461169
Konstantin Ivanovic <121982642+Konstantin907@users.noreply.github.com>
2023-07-03 20:59:47 -0700

    first commit

--- dist/index.44983732.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", ()=>{
-    document.getElementById(".main-menu").classList.add("show");
-});
-
-//# sourceMappingURL=index.44983732.js.map
